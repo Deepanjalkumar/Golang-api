ba54c8f3a250edcbeb714ac610c0d49a8365e9bd
Deepanjal Kumar <55708909+Deepanjalkumar@users.noreply.github.com>
2021-06-10 12:46:58 +0530

    Update main.go

--- main.go
+++ main.go
@@ -12,27 +12,28 @@ func Home_function(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	dms := sources.Alienvault_enum()
 
-	json.NewEncoder(w).Encode(dms)
-}
+	var dom1 []string
 
-func Type_function(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	dms1 := sources.Alienvault_enum()
-	params := mux.Vars(r)
-
-	for index, _ := range dms1 {
-		if dms1[index].Hostname != "" && dms1[index].Record_type == params["type"] {
-			json.NewEncoder(w).Encode(dms1)
-		}
+	for index, _ := range dms {
+		dom1 = append(dom1, dms[index].Hostname)
 	}
 
+	json.NewEncoder(w).Encode(dom1)
+}
+
+func Subdomain_enum(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	dms1 := sources.Anubis_enum()
+
+	json.NewEncoder(w).Encode(dms1)
 }
 
 func main() {
 	mux := mux.NewRouter()
 
 	mux.HandleFunc("/", Home_function).Methods("GET")
-	mux.HandleFunc("/{type}", Type_function).Methods("GET")
+
+	mux.HandleFunc("/subdomain", Subdomain_enum).Methods("GET")
 
 	http.ListenAndServe(":8000", mux)
 
